615be3e91d428a8de0f37b254e33588485e349cf
Antonio <dev.antoniogomez@yahoo.co.uk>
2015-04-21 13:53:10 +0500

    Create sample project for Jenkin and committed first time

---
+++ JenkinTest/JenkinTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinTest
+//
+//  Created by Waseem on 4/21/15.
+//  Copyright (c) 2015 Theappexperts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
